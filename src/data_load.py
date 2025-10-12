ac4828bb26e80ed4bcaae490a198406af9cba29a
Vadim <vadimhus@MacBook-Air-Vadim.local>
2025-10-12 21:57:11 +0300

    feat/bgd: make mini-batch_SGD, change data_load.

--- src/data_load.py
+++ src/data_load.py
@@ -4,7 +4,7 @@ import pandas as pd
 def split_dataset_to_xy(dataset_directory: str = 'housing.csv',
                         target: str = 'median_house_value',
                         frac: float = 0.8) -> tuple:
-    dataset = pd.read_csv(dataset_directory)
+    dataset = remove_nan(pd.read_csv(dataset_directory))
     train_df = dataset.sample(frac=frac, random_state=42)
     test_df = dataset.drop(train_df.index)
 
@@ -13,7 +13,7 @@ def split_dataset_to_xy(dataset_directory: str = 'housing.csv',
     x_test = test_df.drop(columns=[target])
     y_test = test_df[target]
 
-    return add_bias(x_train), add_bias(x_test), y_train, y_test
+    return x_train, x_test, y_train, y_test
 
 
 def add_bias(dataset: pd.DataFrame) -> pd.DataFrame:
@@ -43,7 +43,31 @@ def remove_nan(dataset: pd.DataFrame) -> pd.DataFrame:
     return dataset
 
 
-if __name__ == "__main__":
-    df = pd.read_csv('housing.csv')
-    print(df.head(10))
+def normalization(dataset: pd.DataFrame) -> pd.DataFrame:
+    num_feats = dataset.select_dtypes(include=['int64', 'float64'])
+    cat_feats = dataset.select_dtypes(exclude=['int64', 'float64'])
 
+    num_scaled = (num_feats - num_feats.min()) / (num_feats.max() - num_feats.min())
+
+    dataset_normalized = pd.concat([num_scaled, cat_feats], axis=1)
+
+    return dataset_normalized
+
+
+def standardization(dataset: pd.DataFrame) -> pd.DataFrame:
+    num_feats = dataset.select_dtypes(include=['int64', 'float64'])
+    cat_feats = dataset.select_dtypes(exclude=['int64', 'float64'])
+
+    num_mean = num_feats.mean()
+    n = len(num_feats)
+    num_deviation = (((num_feats - num_mean) ** 2).sum() / (n - 1)) ** 0.5
+
+    num_standard = (num_feats - num_mean) / num_deviation
+
+    dataset_standard = pd.concat([num_standard, cat_feats], axis=1)
+    return dataset_standard
+
+
+if __name__ == "__main__":
+    df = split_dataset_to_xy()
+    print((df[0]))
